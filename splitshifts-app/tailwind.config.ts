8ab5363d3713f0c4534336fb39e0e6ad38e0594a
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-08-09 01:39:18 -0700

    add: Add Tailwind CSS config color light mode palette

--- splitshifts-app/tailwind.config.ts
+++ splitshifts-app/tailwind.config.ts
@@ -7,6 +7,47 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      /**
+       * Light Mode
+       */
+      primary: "#35618E",
+      "on-primary": "#FFFFFF",
+      "primary-container": "#D1E4FF",
+      "on-primary-container": "#001D36",
+
+      secondary: "#535F70",
+      "on-secondary": "#FFFFFF",
+      "secondary-container": "#D6E3F7",
+      "on-secondary-container": "#101C2B",
+
+      tertiary: "#29638A",
+      "on-tertiary": "#FFFFFF",
+      "tertiary-container": "#CBE6FF",
+      "on-tertiary-container": "#001E30",
+
+      error: "#BA1A1A",
+      "on-error": "#FFFFFF",
+      "error-container": "#FFDAD6",
+      "on-error-container": "#410002",
+
+      surface: "#F7F9FF",
+      "on-surface": "#181C20",
+      "on-surface-variant": "#3F4947",
+
+      outline: "#6F7977",
+      "outline-variant": "#BEC9C6",
+      shadow: "#000000",
+      scrim: "#000000",
+      "inverse-surface": "#2D3135",
+      "inverse-on-surface": "#EEF1F6",
+
+      "surface-container-lowest": "#FFFFFF",
+      "surface-container-low": "#F1F4F9",
+      "surface-container": "#EBEEF3",
+      "surface-container-high": "#E5E8ED",
+      "surface-container-highest": "#E0E3E8",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
