3e2cf59624e06db87edd32f3b2b51d7e73a783dd
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-08-09 02:05:57 -0700

    fix: prettier tailwind sorter is now fixed

--- splitshifts-app/src/app/page.tsx
+++ splitshifts-app/src/app/page.tsx
@@ -2,6 +2,10 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
+    <main className='flex flex-col items-center justify-between min-h-screen p-24'>
+      <button className='px-4 py-2 text-white bg-sky-700 hover:bg-sky-800 sm:px-8 sm:py-3'>
+        ...
+      </button>
+    </main>
   );
 }
