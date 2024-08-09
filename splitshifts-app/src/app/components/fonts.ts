9ebbc3b867f4dfd16af48fdbbb241976ffd0a932
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-08-09 01:42:24 -0700

    feat: Add fonts.ts file for custom font imports

---
+++ splitshifts-app/src/app/components/fonts.ts
@@ -0,0 +1,14 @@
+import { Inter } from "next/font/google";
+import { Space_Grotesk } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
+
+export const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-space-grotesk",
+});
